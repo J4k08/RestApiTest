7d5a164bfb3cd9acaa6da23c410cd303d6ffb000
Jakob Haglöf <j.haglof56@gmail.com>
2017-09-27 13:59:29 +0200

    adds react component, database, node and pretty much the test application

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const mongoose   = require('mongoose');
 // set up express app
 const app = express();
 
-// initialize routes
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago', {useMongoClient:true});
+mongoose.Promise = global.Promise;
 
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+// initialize routes
 app.use('/api', require('./routes/api'));
 
-// listen for requests
+// error handling middleware
+app.use(function(error, request, response, next) {
+   //console.log(error);
+    response.status(422).send({errorMessage: error.message});
+});
 
+// listen for requests
 app.listen(process.env.port || 4000, function() {
     console.log('now listenin for requests');
 })
--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,47 @@
 const express   = require('express');
 const router    = express.Router();
+const Ninja     = require('../models/ninja');
 
 // get a list of ninjas from the db
-router.get('/ninjas', function(request, response) {
-    response.send({type: 'GET'});
-});
+router.get('/ninjas', function(request, response, next) {
 
-// add a ninja to the db
-router.post('/ninjas', function(request, response) {
-    console.log(request.body);
-    res.send({
-        type: 'POST',
-        name: request.body.name,
-        rank: request.body.rank
+    // Get all ninjas
+    /*Ninja.find({}).then(function(promise){
+        response.send(promise);
+    });*/
+    Ninja.geoNear(
+        {type:"Point", coordinates: [parseFloat(request.query.lng), parseFloat(request.query.lat)]},
+        {maxDistance: 100000, spherical: true}
+
+
+    ).then(function(promise) {
+        response.send(promise);
     });
 });
 
-// update a ninja in the db
-router.put('ninjas/:id', function(request, response) {
-    console.log(request.body);
+// add a ninja to the db
+router.post('/ninjas', function(request, response, next) {
+
+    Ninja.create(request.body).then(function(promise) {
+        response.send(promise);
+    }).catch(next)
 });
 
-router.delete('ninjas/:id', function(request, response) {
-    console.log(request.body);
+// update a ninja in the db
+router.put('/ninjas/:id', function(request, response, next) {
+    Ninja.findByIdAndUpdate({_id: request.params.id}, request.body).then(function(promise){
+        Ninja.findOne({_id: request.params.id}).then(function(promise) {
+            response.send(promise);
+        })
+
+    })
+});
+
+// delete a ninja from the db
+router.delete('/ninjas/:id', function(request, response, next) {
+    Ninja.findByIdAndRemove({_id: request.params.id}).then(function(promise) {
+        response.send(promise);
+    });
 })
 
 module.exports = router;
